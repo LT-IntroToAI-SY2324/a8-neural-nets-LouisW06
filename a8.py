464ec348a2b303cdc37ee7aeab1250c7014dfbfc
Louis Woodley <lwoodley@cps.edu>
2024-04-22 11:05:20 -0500

    a8 xor data

--- a8.py
+++ a8.py
@@ -1,7 +1,19 @@
-from neural import *
+from neural import NeuralNet
 
 print("<<<<<<<<<<<<<< XOR >>>>>>>>>>>>>>\n")
 
+xor_tdata = [
+    ([0, 0], [0]),
+    ([1, 0], [1]),
+    ([0, 1], [1]),
+    ([0, 0], [0]),
+]
 
 
+xor_neuralnet = NeuralNet(2, 2, 1)
 
+xor_neuralnet.train(xor_tdata, iters = 1000, print_interval = 1000)
+
+print(xor_neuralnet.test_with_expected(xor_tdata))
+
+print("<<<<<<<<<<<<<< XOR >>>>>>>>>>>>>>\n")
